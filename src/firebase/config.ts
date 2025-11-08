9b9d1ec45aacdd1ad7aeeaa358b80661e8a4c92c
Lucky032 <laxmankarnam04@gmail.com>
2025-11-08 05:33:15 +0000

    showing starting the app taking a while

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5566466355-519ba",
-  "appId": "1:598653329347:web:6f0310dc6a167377332776",
-  "apiKey": "AIzaSyAl_Uj6yvHMgGE3CCvfeK5BS_Bbp0Gwqoo",
-  "authDomain": "studio-5566466355-519ba.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "598653329347"
-};
